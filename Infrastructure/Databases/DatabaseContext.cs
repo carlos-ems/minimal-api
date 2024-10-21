06e58e2aebfed7309f48f9bc06ba8510c6da6e3c
carlos <cmsilva1601@gmail.com>
2024-10-20 23:51:37 -0300

    Conexão com o banco de dados

--- Infrastructure/Databases/DatabaseContext.cs
+++ Infrastructure/Databases/DatabaseContext.cs
@@ -1,6 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Entity;
+
 namespace minimal_api.Databases;
 
-public class DatabaseContext
+public class DatabaseContext : DbContext
 {
-    
+    private readonly IConfiguration _configureAppSettings;
+
+    public DatabaseContext(IConfiguration configureAppSettings)
+    {
+        _configureAppSettings = configureAppSettings;
+    }
+
+    public DbSet<Admin> Admins { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configureAppSettings.GetConnectionString("mysql")?.ToString();
+
+            if(!string.IsNullOrEmpty(connectionString))
+            {
+                optionsBuilder.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString)
+                ); 
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Databases;
 using minimal_api.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DatabaseContext>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
